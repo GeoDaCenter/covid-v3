848306625ed50bcf371d26d3cc1304ec8b56c674
Dylan <dhalpern@uchicago.edu>
2022-04-05 10:13:03 -0500

    Dispatch can load in bg after stop playing

--- src/hooks/useTickUpdate.js
+++ src/hooks/useTickUpdate.js
@@ -1,32 +1,51 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { useState, useEffect } from 'react';
-import { incrementDate } from '../actions';
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { incrementDate } from "../actions";
 
-export default function useTickUpdate({
-  currDatesAvailable
-}) {
-  const isTicking = useSelector(({data}) => data.isTicking)
+export default function useTickUpdate({ currDatesAvailable }) {
+  const isTicking = useSelector(({ data }) => data.isTicking);
   const dispatch = useDispatch();
-  const setIsTicking = (bool) => dispatch({type: 'SET_TICKING', payload: bool})
+  const setIsTicking = (bool) =>
+    dispatch({ type: "SET_TICKING", payload: bool });
 
-  const nIndex = useSelector(({params}) => params.dataParams.nIndex);
-  const mapType = useSelector(({params}) => params.mapParams.mapType);
+  const nIndex = useSelector(({ params }) => params.dataParams.nIndex);
+  const mapType = useSelector(({ params }) => params.mapParams.mapType);
   // const [isTicking, setIsTicking] = useState(false);
   const [tickTimer, setTickTimer] = useState(100);
   const [tickTimeout, setTickTimeout] = useState();
   const [resetTimeout, setResetTimeout] = useState();
 
-
   useEffect(() => {
-    if (isTicking) dispatch(incrementDate(1, currDatesAvailable));
+    if (isTicking) {
+      dispatch(incrementDate(1, currDatesAvailable));
+    }
+    if (!isTicking) {
+      dispatch({
+        type: "SET_CAN_LOAD_IN_BACKGROUND",
+        payload: true,
+      });
+    }
   }, [isTicking]);
 
   useEffect(() => {
     if (isTicking) {
       clearTimeout(tickTimeout);
-      setTickTimeout(setTimeout(() => dispatch(incrementDate(1, currDatesAvailable)), tickTimer));
+      setTickTimeout(
+        setTimeout(
+          () => dispatch(incrementDate(1, currDatesAvailable)),
+          tickTimer
+        )
+      );
       clearTimeout(resetTimeout);
-      setResetTimeout(setTimeout(() => setIsTicking(false), 1500));
+      setResetTimeout(
+        setTimeout(() => {
+          setIsTicking(false);
+          dispatch({
+            type: "SET_CAN_LOAD_IN_BACKGROUND",
+            payload: true,
+          });
+        }, 1500)
+      );
     }
   }, [nIndex]);
 
