a570e81572bdc9a4cea4999e21a6cd7d323a9a57
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-11-14 14:20:25 -0600

    Date error fix

--- src/components/Interface/Slider.jsx
+++ src/components/Interface/Slider.jsx
@@ -226,7 +226,8 @@ function DateTitle({
     const lastDateIdx = [...currDatesAvailable].reverse().indexOf(1)
     const minDate = new Date(dates[firstDateIdx] || '2020-01-01')
     const maxDate = new Date(dates.slice(-lastDateIdx)[0] || '')
-    const currDate = currIndex === null ? maxDate : new Date(dates[currIndex+1] || '2020-01-01')
+    const currDate = new Date(dates[currIndex] || '2020-01-01')
+
     const onChange =
         rangeType === 'custom'
             ? (date, position) => {
