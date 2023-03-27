f5531241701db27f4b89bccda735b4863aee3d40
Adam Cox <mr.adamcox@gmail.com>
2023-03-27 12:24:09 -0500

    acquire MAPBOX_ACCESS_TOKEN from env

--- src/config/mbToken.js
+++ src/config/mbToken.js
@@ -5,5 +5,4 @@
  * @category Configuration
  * @type {string}
  */
-export const MAPBOX_ACCESS_TOKEN =
- 'pk.eyJ1IjoibGl4dW45MTAiLCJhIjoiY2locXMxcWFqMDAwenQ0bTFhaTZmbnRwaiJ9.VRNeNnyb96Eo-CorkJmIqg'
+export const MAPBOX_ACCESS_TOKEN = `${process.env.MAPBOX_ACCESS_TOKEN}`;
