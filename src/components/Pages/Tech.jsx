eb81a64caa273899b1b7cdeb4d14c7e9bf8d136f
Adam Cox <mr.adamcox@gmail.com>
2024-07-29 13:04:32 -0500

    add tech page content

--- src/components/Pages/Tech.jsx
+++ src/components/Pages/Tech.jsx
@@ -32,10 +32,16 @@ const Methodology = () => {
       <ContentContainer>
         <h1>Tech</h1>
         <hr />
-        <h3>Our Stack</h3>
+        <h3>Overall Architecture</h3>
         <br />
         <p>
-          The US Covid Atlas is made from a few different components:
+          The US Covid Atlas is a static web application built with React and deployed via Netlify. Through the course of the project, GitHub actions were used to run Python scraping scripts that fetched, processed, and recombined Covid-related data from <a href="/docs">many different sources</a>. This data was then zipped and pushed to AWS S3 and Google BigQuery. The build process for the site downloads and unzips these files, making them available for access by the frontend map and other visualization tools. A Google Sheet, which is also ingested at build time, provides a user-friendly configuration format that holds variable names, map symbology information, and more.
+        </p>
+        <Gutter h={40} />
+        <h3>Geospatial Analysis and Visualization</h3>
+        <br />
+        <p>
+          The interactive map and charts in the US Covid Atlas are made from a few different components:
           <ul>
             <li>
               <strong><a href="https://xunli.gitbook.io/jsgeoda">jsGeoDa</a></strong> &mdash; a JavaScript library that performs advanced spatial analysis operations directly in your web browser. jsGeoDa is built from a <a href="https://developer.mozilla.org/en-US/docs/WebAssembly">WebAssembly</a> port of <a href="https://geodacenter.github.io/">GeoDa</a>, an open source spatial analysis software developed and maintained by the GeoDaCenter at University of Chicago. For this application, jsGeoDa has been <a href="https://github.com/GeoDaCenter/jsgeoda/tree/worker-compatible">modified slightly</a>.
@@ -52,22 +58,24 @@ const Methodology = () => {
           </ul>
         </p>
         <Gutter h={40} />
-        <br />
         <h3>Open Source</h3>
         <br />
         <p>
-          The Github for our project is public and available at{' '}
+          The Github repository for the current release of the US Covid Atlas is public and available at{' '}
           <a
             href="https://github.com/GeoDaCenter/covid-v3"
             target="_blank"
             rel="noopener noreferrer"
           >
             github.com/GeoDaCenter/covid-v3
-          </a>.{' '}
-          Contributions are welcome. Volunteers are encouraged to start on the
-          “Issues” page and identify “good first issues” to maximize help
-          efforts. A public release of the application will continue to be
-          updated with new data and features on a regular basis.
+          </a>.{' '} The full archive of our historical Covid datasets is in the git history of our archive repo{' '}
+          <a
+            href="https://github.com/GeoDaCenter/covid"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github.com/GeoDaCenter/covid
+          </a>.
         </p>
       </ContentContainer>
       <Footer />
