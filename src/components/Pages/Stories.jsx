b0311d814c4222f403946352beddf7eac2db983b
Sara Lambert <lambert8@illinois.edu>
2024-09-17 17:23:58 -0500

    fix: update placeholder text

--- src/components/Pages/Stories.jsx
+++ src/components/Pages/Stories.jsx
@@ -65,7 +65,13 @@ export default function Stories() {
                     />*/}
                 </Grid>
                 <Grid container spacing={3}>
-                    <Typography>Page contents</Typography>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </Typography>
                 </Grid>
             </ContentContainer>
             <Footer />
