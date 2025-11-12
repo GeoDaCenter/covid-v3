1019cf6fc11aec4449a434fb9bc3bebea5a6f52d
Adam Cox <mr.adamcox@gmail.com>
2025-11-12 11:09:21 -0600

    add update controls on video player

--- src/components/Stories/StoryPlayer.jsx
+++ src/components/Stories/StoryPlayer.jsx
@@ -38,8 +38,26 @@ function StoryPlayer({
                     }
                 ]
             };
+            const controls = [
+                // 'play-large', // The large play button in the center
+                // 'restart', // Restart playback
+                // 'rewind', // Rewind by the seek time (default 10 seconds)
+                'play', // Play/pause playback
+                // 'fast-forward', // Fast forward by the seek time (default 10 seconds)
+                'progress', // The progress bar and scrubber for playback and buffering
+                'current-time', // The current time of playback
+                'duration', // The full duration of the media
+                // 'mute', // Toggle mute
+                'volume', // Volume control
+                'captions', // Toggle captions
+                'settings', // Settings menu
+                'pip', // Picture-in-picture (currently Safari only)
+                // 'airplay', // Airplay (currently Safari only)
+                'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
+                'fullscreen', // Toggle fullscreen
+            ]
             return <>
-                <div><Plyr source={videoSrc} crossOrigin="anonymous" /></div>
+                <div><Plyr source={videoSrc} crossOrigin="anonymous" options={{controls:controls}} /></div>
                 {story.shortPresent &&
                     <button style={{color:'white', padding: '5px'}} onClick={() => {setShowShort(!showShort)}}>
                         Show {showShort ? 'full' : 'short'} video
